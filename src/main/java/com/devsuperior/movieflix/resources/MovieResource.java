07607a7c264ba2baeb901397d85233a23e501c5f
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2021-11-10 14:27:58 -0300

    Find Movies by genres and MovieMinDTO implemented and fixes

--- src/main/java/com/devsuperior/movieflix/resources/MovieResource.java
+++ src/main/java/com/devsuperior/movieflix/resources/MovieResource.java
@@ -9,9 +9,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.dto.MovieMinDTO;
 import com.devsuperior.movieflix.dto.ReviewDTO;
 import com.devsuperior.movieflix.services.MovieService;
 import com.devsuperior.movieflix.services.ReviewService;
@@ -26,11 +28,11 @@ public class MovieResource {
 	@Autowired
 	private ReviewService reviewService;
 	
-	@GetMapping
-	public ResponseEntity<Page<MovieDTO>> findAll(Pageable pageable) {
-		Page<MovieDTO> page = movieService.findAllPaged(pageable);
-		return ResponseEntity.ok().body(page);
-	}
+//	@GetMapping
+//	public ResponseEntity<Page<MovieDTO>> findAll(Pageable pageable) {
+//		Page<MovieDTO> page = movieService.findAllPaged(pageable);
+//		return ResponseEntity.ok().body(page);
+//	}
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<MovieDTO> findById(@PathVariable Long id) {
@@ -42,5 +44,12 @@ public class MovieResource {
 	public ResponseEntity<List<ReviewDTO>> findReviewsByMovie(@PathVariable Long movieId) {
 		List<ReviewDTO> list = reviewService.findReviewsByMovie(movieId);
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<MovieMinDTO>> findByGenre(Pageable pageable,
+			@RequestParam(value = "genreId", defaultValue = "0") Long genreId) {
+		Page<MovieMinDTO> page = movieService.findByGenre(pageable, genreId);
+		return ResponseEntity.ok().body(page);
 	}
 }
